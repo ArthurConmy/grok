77f79cb64df6d366f224b14a24731da8d1a1c846
Arthur <arthurconmy@gmail.com>
2022-07-04 15:09:16 +0100

    Automated commit

--- play.py
+++ play.py
@@ -1,3 +1,4 @@
+from tqdm import tqdm
 import torch as t
 import torch.nn.functional as F
 from model.transformer import Transformer, get_transformer
@@ -8,6 +9,8 @@ import seaborn
 import matplotlib.pyplot as plt
 from einops import repeat, rearrange
 OFFSET = 22
+SAVED_MODELS = [0, 30, 60, 90, 120, 150, 400, 700, 850, 880, 910, 940, 970, 999]
+MODEL_FILES = [f"checkpoints/seed1at{saved_model}.pt" for saved_model in SAVED_MODELS]
 
 def get_average_confidence(model, x):
     with t.no_grad():
@@ -16,7 +19,10 @@ def get_average_confidence(model, x):
         max_probs = t.max(probs, dim=1).values
         return t.sum(max_probs).item() / max_probs.shape[0]
 
-for model_file in ["checkpoints/32_train.pt", "checkpoints/32_valid.pt"]:
+ct = []
+cv = []
+
+for model_file in tqdm(MODEL_FILES):
     print(f"Model {model_file}")
     my_model_config = dict(DEFAULT_MODEL_CONFIG)
     my_model_config["num_heads"] = 32
@@ -36,10 +42,6 @@ for model_file in ["checkpoints/32_train.pt", "checkpoints/32_valid.pt"]:
     correct_probs = t.sum(probs * answers, dim=1) 
     prob_matrix = rearrange(correct_probs, "(a b) -> a b", a=97, b=97)
 
-    # a1 = maxes * answers
-    # print("a1 shape", a1.shape)
-    # input()
-
     correct_maxes = t.sum(maxes * answers, dim=1)
     correct_matrix = rearrange(correct_maxes, "(a b) -> a b", a=97, b=97)
 
@@ -52,9 +54,9 @@ for model_file in ["checkpoints/32_train.pt", "checkpoints/32_valid.pt"]:
     )
     for x, y in train_data:
         indices = 97 * (x[:,0]) + x[:,1]
-        print("Training confidence", get_average_confidence(model, x))
+        ct.append(get_average_confidence(model, x))
     for x, y in valid_data:
-        print("Valid confidence", get_average_confidence(model, x))
+        cv.append(get_average_confidence(model, x))
 
     is_tdata = t.zeros(97 * 97).int()
     write_data = t.ones_like(indices).int()
@@ -89,3 +91,7 @@ for model_file in ["checkpoints/32_train.pt", "checkpoints/32_valid.pt"]:
     #         [46, 95],
     #         [88, 42]])
     # tensor([16, 81, 36,  ..., 40, 44, 33])
+
+plt.plot(list(range(len(ct))), ct)
+plt.plot(list(range(len(cv))), cv)
+plt.show()
